a1c345d144f45b6606defb74782bd8b19b9202fe
Ruth Ivimey-Cook <r.ivimeycook@elifesciences.org>
2014-08-01 17:48:45 +0100

    Make the script submit the template.

--- mailcivi.py
+++ mailcivi.py
@@ -57,53 +57,89 @@ def getoptions():
     parser = argparse.ArgumentParser(
         prog='mailcivi',
         description='Create a new mail template in a remote CiviCRM installation.',
-        usage='%(prog)s [options]'
+        usage='%(prog)s [options] [htmlfile] [textfile]'
     )
     parser.add_argument('--url',
-                        help='URL of the site', default='http://crm.example.org/crm/civicrm/ajax/rest')
-    parser.add_argument('--sitekey', help='site_key of the site you are connecting to.', default='')
-    parser.add_argument('--apikey', help='user api key', default='')
-    parser.add_argument('--name', help='name of new template', default='mailinglist')
-    parser.add_argument('--subject', help='Email subject text', default='News')
-    parser.add_argument('--sendto', help='CiviCRM group name to send mail to', default='')
+                        help='URL of the site homepage.', default='http://crm.example.org/sites/all/modules/civicrm')
+    parser.add_argument('--sitekey', help='The site_key of the site you are connecting to.', default='')
+    parser.add_argument('--apikey', help='The api key.', default='')
+    parser.add_argument('--name', help='Name of new template.', default='mailinglist')
+    parser.add_argument('--subject', help='Email subject text.', default='News')
+    parser.add_argument('--from_id', help='CiviCRM Contact ID of sender.', default='1')
     parser.add_argument('htmlfile', nargs='?', type=argparse.FileType('r'),
-                        help='file containing the templated HTML to email.', default=sys.stdin)
+                        help='File containing the templated HTML to email. If not supplied, read from stdin.',
+                        default=sys.stdin)
+    parser.add_argument('textfile', nargs='?', type=argparse.FileType('r'),
+                        help='File containing the templated Text to email. If not supplied, the html version'
+                             'is rendered using html2text.',
+                        default=None)
     parser.add_argument('--verbose', '-v', action='count',
-                        help='print additional messages to stderr', default=0)
+                        help='Print additional messages to stderr', default=0)
 
     args = parser.parse_args()
 
     return args
 
 
-def getPlaintext(html):
-    #html = open("foobar.html").read()
+def getplaintext(html):
     return html2text.html2text(html)
+
+def check_creator(civicrm, creator_id):
+    params = {
+        'contact_id': creator_id,
+    }
+    contactresults = civicrm.get('Contact', **params)
+    debugmsg('Owner is object ', contactresults)
+    if len(contactresults) == 1:
+        if (contactresults[0]['contact_id'] == creator_id):
+            infomsg('Creator is ', contactresults[0]['sort_name'])
+        else:
+            warningmsg('Creator id did not match.')
+            return False
+    else:
+        warningmsg('Creator id was not found.')
+        return False
+
+    return True
+
 
 def main():
     global settings
     settings = getoptions()
 
-    # http://crm.elifesciences.org/crm/sites/all/modules/civicrm/extern/
-    #    rest.php??entity=Mailing&action=get&debug=1&sequential=1&json=1&api_key={yoursitekey}&key={yourkey}
-
-    url = 'http://crm.elifesciences.org/crm'
     htmltemplate = settings.htmlfile.read()
-    plaintext = getPlaintext(htmltemplate)
+    plaintext = getplaintext(htmltemplate)
 
-    civicrm = CiviCRM(url, site_key=settings.sitekey, api_key=settings.apikey, use_ssl=False)
+    infomsg('Using: ')
+    infomsg('  URL  :', settings.url)
+    infomsg('  Skey :', settings.sitekey)
+    infomsg('  Akey :', settings.apikey)
+    infomsg('Name   :', settings.name)
+    infomsg('Subject:', settings.subject)
+    infomsg('Creator:', settings.from_id)
 
-    # use htmltemplate
+    civicrm = CiviCRM(settings.url, site_key=settings.sitekey, api_key=settings.apikey, use_ssl=False)
 
-    search_results = civicrm.get('Mailing')
+    debugmsg('Constructed object ', civicrm)
 
-    print(search_results)
+    if check_creator(civicrm, settings.from_id):
+        params = {
+            'name': settings.name,
+            'subject': settings.subject,
+            'created_id': settings.from_id,
+            'body_html': htmltemplate,
+            'body_text': plaintext,
+            'url_tracking': '1',
+        }
+        results = civicrm.create('Mailing', **params)
+        infomsg('Returned object ', results)
 
 
 try:
     if __name__ == "__main__":
         main()
 except KeyboardInterrupt:
+    print("Interrupted\n")
     sys.exit(1)
 
 
