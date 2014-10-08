f48fe8b277eea09daaa95c4788cc09e734cabd7c
Ruth Ivimey-Cook <r.ivimeycook@elifesciences.org>
2014-10-08 16:30:25 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='elife-civiapi',
-    version='1.0',
+    version='1.0.1',
     install_requires=[
         "requests",
         "html2text",
